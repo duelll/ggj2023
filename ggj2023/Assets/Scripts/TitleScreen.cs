5a22843d834ad067d8a7676c89fc653c0a3c0efd
Luis Hernandez <luisehdz14@gmail.com>
2023-02-03 23:57:57 -0600

    wip ui

--- ggj2023/Assets/Scripts/TitleScreen.cs
+++ ggj2023/Assets/Scripts/TitleScreen.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class TitleScreen : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public string scene;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Button start()
     {
-        
+        SceneManager.LoadScene(scene);
     }
 }
