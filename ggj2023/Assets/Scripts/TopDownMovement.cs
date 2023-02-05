e59971f01a9c0edcac2e9910287366ff8b78a483
Luis Hernandez <luisehdz14@gmail.com>
2023-02-04 21:39:15 -0600

    New Player controls

--- ggj2023/Assets/Scripts/TopDownMovement.cs
+++ ggj2023/Assets/Scripts/TopDownMovement.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class TopDownMovement : MonoBehaviour
 {
-    public float moveSpeed;
-    public Rigidbody2D rb2d;
-    private Vector2 moveInput;
-
     // Start is called before the first frame update
+    
+
+    public float moveSpeed;
+
+    public Rigidbody2D rb2d;
+
+    private Vector2 moveDirection;
     void Awake()
     {
         rb2d = this.gameObject.GetComponent<Rigidbody2D>();
@@ -17,12 +20,26 @@ public class TopDownMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        moveInput.x = Input.GetAxis("Horizontal");
-        moveInput.y = Input.GetAxis("Vertical");
-
-        moveInput.Normalize();
-
-        rb2d.velocity = moveInput * moveSpeed;
+        ProcessInputs();
     }
 
+    void FixedUpdate()
+    {
+        Move();
+
+    }
+
+    void ProcessInputs()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        moveDirection = new Vector2(moveX, moveY).normalized;
+    }
+
+    void Move()
+    {
+        rb2d.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+
+    }
 }
